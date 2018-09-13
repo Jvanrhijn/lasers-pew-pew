61cd4046805b3a86a4c072789068ddd672198554
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-09-13 16:16:31 +0200

    Make Shape a handle class && start to implement Cirlce

--- src/Shape.m
+++ src/Shape.m
@@ -1,12 +1,12 @@
-classdef Shape
+classdef Shape < handle
   properties
     location_    
   end
 
   methods
 
-    function obj = Shape(obj, location)
-      obj.location_ = location;
+    function self = Shape(location)
+      self.location_ = location;
     end
 
     function set_dimensions(self, dims)
@@ -14,7 +14,7 @@ classdef Shape
       % dims is a matlab array of dimensions, differs per shape
       % ex. circle has radius, rectangle width and height, etc
     end
-    
+
     % to interact with a ray, the ray must intersect
     % the shape's surface
     function on = intersects(self, ray)
@@ -24,7 +24,6 @@ classdef Shape
     new_ray = function interact_with(self, obj)
 
     end
-  
 
   end
 
