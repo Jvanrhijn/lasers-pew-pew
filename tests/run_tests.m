78b7e1774bb452f24fe1af38c93814b63ddb2449
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-09-15 19:59:21 +0200

    Implement Cirlce::set_dimensions()

--- tests/run_tests.m
+++ tests/run_tests.m
@@ -6,9 +6,10 @@ import matlab.unittest.TestRunner;
 import matlab.unittest.TestSuite;
 runner = TestRunner.withTextOutput;
 
-suites = [TestSuite.fromClass(?TestVec)];
+suites = {TestSuite.fromClass(?TestVec),
+          TestSuite.fromClass(?TestCircle)};
 
 
 for i=1:length(suites)
-  runner.run(suites(i));
+  runner.run(suites{i});
 end
