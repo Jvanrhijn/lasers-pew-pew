146304c78e33b4626664ab79e80fefc4e0e54551
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-09-15 19:59:21 +0200

    Implement Cirlce::set_dimensions()

--- src/Circle.m
+++ src/Circle.m
@@ -1,11 +1,19 @@
 classdef Circle < Shape
 
   properties
-
+    radius_
   end
 
   methods
-      
+
+    function set_dimensions(self, dims)
+      self.radius_ = dims(1);
+    end
+
+    function on = intersects(self, ray)
+      on = false;
+    end
+
   end
 
 end
--- tests/TestCircle.m
+++ tests/TestCircle.m
@@ -10,7 +10,12 @@ classdef TestCircle < matlab.unittest.TestCase
 
   methods(Test)
 
-    % ... etc
+    function test_intersect(self)
+      circle = Circle(Vec(0, 0));
+      circle.set_dimensions(1); % circle radius
+      ray = Ray(Vec(0, 0), 0); % ray starting in origin
+      self.assertTrue(circle.intersects(ray)); 
+    end
 
   end
 
