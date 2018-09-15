92b44c2f74db1d5cb669ab9197224f9b500f7e2e
Jesse van Rhijn <jesse.v.rhijn@gmail.com>
2018-09-15 20:57:10 +0200

    Attempt implementation of Circle::intersects()

--- tests/TestCircle.m
+++ tests/TestCircle.m
@@ -10,11 +10,43 @@ classdef TestCircle < matlab.unittest.TestCase
 
   methods(Test)
 
-    function test_intersect(self)
-      circle = Circle(Vec(0, 0));
-      circle.set_dimensions(1); % circle radius
-      ray = Ray(Vec(0, 0), 0); % ray starting in origin
-      self.assertTrue(circle.intersects(ray)); 
+    function test_intersect_from_center(self)
+      for i=1:10
+        x = rand();
+        y = rand();
+        circle = Circle(Vec(x, y));
+        circle.set_dimensions(1); % circle radius
+        ray = Ray(Vec(x, y), rand()); % ray starting in circle centroid
+        self.assertTrue(circle.intersects(ray)); 
+      end
+    end
+
+    function test_not_intersecting(self)
+      for i=1:10
+        x = rand();
+        y = rand();
+        radius = 0.1;
+        circle = Circle(Vec(x, y));
+        circle.set_dimensions(radius);
+        ray = Ray(Vec(x+rand(), y+radius+abs(rand())), 0);
+        self.assertFalse(circle.intersects(ray));
+      end
+    end
+
+    function test_intersects_from_outside(self)
+      for i=1:10
+        x = rand();
+        y = rand();
+        radius = 0.1;
+        center = Vec(x, y);
+        circle = Circle(center);
+        circle.set_dimensions(radius);
+        % ray going through centerpoint
+        ray_center = Ray(Vec(0, 0), center.angle(Vec(1, 0)));
+        self.assertTrue(circle.intersects(ray_center));
+        % more generally:
+        % TBI
+      end
     end
 
   end
